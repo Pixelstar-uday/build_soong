8454e6154be4f3b23da1fc9745f64094344777e7
HeroBuxx <me@buxxed.tk>
2023-02-04 13:21:31 +0000

    soong: config: Allow Perl

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -111,6 +111,7 @@ var Configuration = map[string]PathConfig{
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"patch":   Allowed,
+	"perl":   Allowed,
 	"pkg-config": Allowed,
 	"pstree":  Allowed,
 	"python3": Allowed,
